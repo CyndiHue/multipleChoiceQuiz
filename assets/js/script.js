c6e0560c95a3b7b706a7f17d5c2f3405bb29d0b1
CyndiCodes <huertascyn@gmail.com>
2023-07-31 16:04:11 -0400

    adding start button, currently not working but displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,44 @@
-let timer = 60;  
+let timer = 101;  
 let quizDiv = document.querySelector("#quiz");
-let questionButton1 = document.querySelector("#answer1");
-let questionButton2 = document.querySelector("#answer2");
-let questionButton3 = document.querySelector("#answer3");
-let questionButton4 = document.querySelector("#answer4");
+let button1 = document.querySelector("#answer1");
+let button2 = document.querySelector("#answer2");
+let button3 = document.querySelector("#answer3");
+let button4 = document.querySelector("#answer4");
 let quizQuestion = document.querySelector("#question");
 let timerEl = document.querySelector("#timeLeft")
-
- let timerId = setInterval(function(){
-    timer -=1;
-    console.log(timer)
-    timerEl.textContent = timer;
-      
-  }, 1000)
-
-// timer=timer-=10;when user gets answer wrong;
-// clearInterval(timerID to stop timer);
-
-
-let examQuestions = [{question:"question 1", answers: ["choice 1", "choice 2", "choice 3", "choice 4"], correctAnswer:"choice 4"},
-{question:"question 2", answers: ["choice 5", "choice 6", "choice 7", "choice 8"], correctAnswer:"choice 8"},
-{question:"question 3", answers: ["choice 9", "choice 10", "choice 11", "choice 12"], correctAnswer:"choice 12"},
-{question:"question 4", answers: ["choice 13", "choice 14", "choice 15", "choice 16"], correctAnswer:"choice 16"}];
-
-
 let currentQuestion = 0;
-renderQuestion()
 
+let timerId = setInterval(function(){
+  timer --;
+  console.log(timer);
+  timerEl.textContent = timer;
+    if (timer === 0) {
+      clearInterval(timerId)
+    }
+}, 1000)
+  
+  
+let examQuestions = [{question:"question 1", answers: ["choice 1", "choice 2", "choice 3", "choice 4"], correctAnswer:"choice 4"},
+  {question:"question 2", answers: ["choice 5", "choice 6", "choice 7", "choice 8"], correctAnswer:"choice 8"},
+  {question:"question 3", answers: ["choice 9", "choice 10", "choice 11", "choice 12"], correctAnswer:"choice 12"},
+  {question:"question 4", answers: ["choice 13", "choice 14", "choice 15", "choice 16"], correctAnswer:"choice 16"}];
+  
+let questionSelector = examQuestions[currentQuestion];
+  
+renderQuestion()
+currentQuestion++;
+  
 function renderQuestion(){
-  quizQuestion.textContent = (examQuestions[currentQuestion].question);
-  questionButton1.textContent = (examQuestions[currentQuestion].answers[0]);
-  questionButton2.textContent = (examQuestions[currentQuestion].answers[1]);
-  questionButton3.textContent = (examQuestions[currentQuestion].answers[2]); 
-  questionButton4.textContent = (examQuestions[currentQuestion].answers[3]);
-  console.log(examQuestions[currentQuestion].question);
-  console.log(examQuestions[currentQuestion].answers[0]);
-  console.log(examQuestions[currentQuestion].answers[1]);
-  console.log(examQuestions[currentQuestion].answers[2]); 
-  console.log(examQuestions[currentQuestion].answers[3]);
+  quizQuestion.textContent = (questionSelector.question);
+  button1.textContent = (questionSelector.answers[0]);
+  button2.textContent = (questionSelector.answers[1]);
+  button3.textContent = (questionSelector.answers[2]); 
+  button4.textContent = (questionSelector.answers[3]);
+  console.log(questionSelector.question);
+  console.log(questionSelector.answers[0]);
+  console.log(questionSelector.answers[1]);
+  console.log(questionSelector.answers[2]); 
+  console.log(questionSelector.answers[3]);
   
   }
   quizDiv.addEventListener("click", function(event){
@@ -62,57 +63,3 @@ function renderQuestion(){
       renderQuestion();
       currentQuestion++;
 }});
-currentQuestion++;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var timerEl = document.getElementById('countdown');
-// var message = "";
-
-// function countdown() {
-//     var timeLeft = 5;
-//     timerEl.textContent = timeLeft;
-//     var timeInterval = setInterval(function () {
-//       timeLeft--;
-//       timerEl.textContent = timeLeft;
-      
-//       if (timeLeft === 0){
-//         clearInterval(timeInterval);
-//         displayMessage(); 
-//       }
-      
-//      }, 1000);
-//   };
-
-//   function displayMessage() {
-//     var wordCount = 0;
-  
-//     // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//     var msgInterval = setInterval(function () {
-//       // If there are no more words left in the message
-//       if (words[wordCount] === undefined) {
-//         // Use `clearInterval()` to stop the timer
-//         clearInterval(msgInterval);
-//       } else {
-//         // Display one word of the message
-//         mainEl.textContent = words[wordCount];
-//         wordCount++;
-//       }
-//     }, 1000);
-//   }
-  
-//   countdown();
