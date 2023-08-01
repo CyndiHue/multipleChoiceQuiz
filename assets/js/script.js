d5c36ec852c3c9ac494433274e43fe99e84528c5
CyndiCodes <huertascyn@gmail.com>
2023-07-31 20:15:12 -0400

    start button functional, everything working correctly BUT must display score on screen and show game over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let timer = 101;  
+let timer = 61;  
 let quizDiv = document.querySelector("#quiz");
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
@@ -6,29 +6,45 @@ let questionButton3 = document.querySelector("#answer3");
 let questionButton4 = document.querySelector("#answer4");
 let quizQuestion = document.querySelector("#question");
 let timerEl = document.querySelector("#timeLeft")
-let startQuizButton = document.querySelector("#startQuiz")
 let correctMessage = document.querySelector("displayMessage")
 let currentQuestion = 0;
+let timerReset = 0;
+let userScore = 0;
 
-let timerId = setInterval(function(){
+
+let startQuizButton = document.querySelector("#startQuiz");
+
+startQuizButton.addEventListener("click", startQuiz);
+
+
+function startQuiz(){
+  startQuizButton.style.display = "none";
+  
+  let timerId = setInterval(function(){
     timer --;
     console.log(timer);
     timerEl.textContent = timer;
-    if (timer === 0) {
-      clearInterval(timerId)
+    if (timer === 0 || timer <0) {
+    clearInterval(timerId)
     }
-    
+  
   }, 1000)
 
-let examQuestions = [{question:"question 1", answers: ["choice 1", "choice 2", "choice 3", "choice 4"], correctAnswer:"choice 4"},
-{question:"question 2", answers: ["choice 5", "choice 6", "choice 7", "choice 8"], correctAnswer:"choice 8"},
-{question:"question 3", answers: ["choice 9", "choice 10", "choice 11", "choice 12"], correctAnswer:"choice 12"},
-{question:"question 4", answers: ["choice 13", "choice 14", "choice 15", "choice 16"], correctAnswer:"choice 16"}];
+  let examQuestions = [{question:"What animal is Blue's Clues", answers: ["cat", "dog", "rat", "lizard"], correctAnswer:"dog"},
+  {question:"Peppa the Pig hung up on the sheep because he could...", answers: ["smile", "dance", "whistle", "run"], correctAnswer:"whistle"},
+  {question:"What is Ratatouille's skill?", answers: ["chef", "body builder", "poet", "teacher"], correctAnswer:"chef"},
+  {question:"In Finding Nemo, Nemo lives in the...", answers: ["forest", "jungle", "city", "ocean"], correctAnswer:"ocean"},
+  {question:"The talking cow in Mulan was really a...", answers: ["hippo", "horse", "dragon", "bee"], correctAnswer:"horse"},
+  {question:"Cinderella was friends with how many dwarves?", answers: ["seven", "four", "nine", "ten"], correctAnswer:"seven"},
+  {question:"What broke the curse of sleeping beauty?", answers: ["sleep", "exercise", "makeup", "a kiss"], correctAnswer:"a kiss"},
+  {question:"Land before time was about?", answers: ["lions", "dinosaurs", "lobsters", "humans"], correctAnswer:"dinosaurs"},
+  {question:"Simba fell in love with...", answers: ["Amy", "Clara", "Nala", "Bella"], correctAnswer:"Nala"},
+  {question:"Finish the title: Beauty and the...", answers: ["Rabbit", "Ogre", "Prince", "Beast"], correctAnswer:"Beast"}];
 
 
-renderQuestion()
+  renderQuestion()
 
-function renderQuestion(){
+  function renderQuestion(){
   quizQuestion.textContent = (examQuestions[currentQuestion].question);
   questionButton1.textContent = (examQuestions[currentQuestion].answers[0]);
   questionButton2.textContent = (examQuestions[currentQuestion].answers[1]);
@@ -41,26 +57,36 @@ function renderQuestion(){
   console.log(examQuestions[currentQuestion].answers[3]);
   
   }
+
   quizDiv.addEventListener("click", function(event){
-    let element = event.target;
+  let element = event.target;
     let questionAnswer = examQuestions[currentQuestion-1].correctAnswer;
     
     if (element.matches("button")){
       let userChoice = element.textContent;
+      // textContent turns button id to answer selected
       
       console.log("your answer:" + userChoice);
       console.log("correct answer:" + questionAnswer);
-    
+      // for building/editing purposes. remove once deployed 
+      
     if (userChoice === questionAnswer){
-      console.log("Correct!")
+        console.log("Correct!")
       alert("Correct!")
     }else {
       console.log("whoops")
       alert("whoops...")
       timer-=5;
     }
-    
       renderQuestion();
       currentQuestion++;
-}});
-currentQuestion++;
+      
+      if (userChoice === questionAnswer){
+        userScore++;
+        console.log(userScore + "/10")
+    }
+
+    
+  }});
+  currentQuestion++;
+}
