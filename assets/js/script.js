a376d678d589fe7ef93207adfe3ba0dd4774dc26
CyndiCodes <huertascyn@gmail.com>
2023-08-01 17:36:16 -0400

    all working except adding array for local storage and displaying high scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,30 @@
-let timer = 61;  
 let quizDiv = document.querySelector("#quiz");
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
 let questionButton3 = document.querySelector("#answer3");
 let questionButton4 = document.querySelector("#answer4");
 let quizQuestion = document.querySelector("#question");
-let correctMessage = document.querySelector("displayMessage");
-let currentQuestion = 0;
-let userScore = 0;
 let timerEl = document.querySelector("#timeLeft");
 let scoreEl = document.querySelector("#score");
+let endDiv = document.querySelector("#end")
+// below El currently not being used
+let textEl = document.querySelector("#text")
+let submitEl = document.querySelector("#submit-button")
 
 let startQuizButton = document.querySelector("#startQuiz");
 startQuizButton.addEventListener("click", startQuiz);
 
+let timer = 61;  
+let currentQuestion = 0;
+let userScore = 0;
+
 quizDiv.style.display = "none";
+endDiv.style.display = "none"
 
 function startQuiz(){
   startQuizButton.style.display = "none";
   quizDiv.style.display = "block";
+  endDiv.style.display = "none"
   
   let timerId = setInterval(function(){
     timer --;
@@ -31,22 +37,31 @@ function startQuiz(){
   
   }, 1000)
 
-let examQuestions = [{question:"What animal is Blue's Clues", answers: ["cat", "dog", "rat", "lizard"], correctAnswer:"dog"},
-{question:"Peppa the Pig hung up on the sheep because they could...", answers: ["smile", "dance", "whistle", "run"], correctAnswer:"whistle"},
-{question:"What is Ratatouille's skill?", answers: ["chef", "body builder", "poet", "teacher"], correctAnswer:"chef"},
-{question:"In Finding Nemo, Nemo lives in the...", answers: ["forest", "jungle", "city", "ocean"], correctAnswer:"ocean"},
-{question:"The 'cow' in Mulan was really a...", answers: ["hippo", "horse", "dragon", "bee"], correctAnswer:"horse"},
-{question:"Snow White was friends with how many dwarves?", answers: ["seven", "four", "nine", "ten"], correctAnswer:"seven"},
-{question:"What broke the curse of sleeping beauty?", answers: ["sleep", "exercise", "makeup", "a kiss"], correctAnswer:"a kiss"},
-{question:"Land before time was about?", answers: ["lions", "dinosaurs", "lobsters", "humans"], correctAnswer:"dinosaurs"},
-{question:"Simba fell in love with...", answers: ["Amy", "Clara", "Nala", "Bella"], correctAnswer:"Nala"},
-{question:"Finish the title: Beauty and the...", answers: ["Rabbit", "Ogre", "Prince", "Beast"], correctAnswer:"Beast"}];
+  let examQuestions = [{question:"What animal is Blue's Clues", answers: ["cat", "dog", "rat", "lizard"], correctAnswer:"dog"},
+  {question:"Peppa the Pig hung up on the sheep because they could...", answers: ["smile", "dance", "whistle", "run"], correctAnswer:"whistle"},
+  {question:"What is Ratatouille's skill?", answers: ["chef", "body builder", "poet", "teacher"], correctAnswer:"chef"},
+  {question:"In Finding Nemo, Nemo lives in the...", answers: ["forest", "jungle", "city", "ocean"], correctAnswer:"ocean"},
+  {question:"The 'cow' in Mulan was really a...", answers: ["hippo", "horse", "dragon", "bee"], correctAnswer:"horse"},
+  {question:"Snow White was friends with how many dwarves?", answers: ["seven", "four", "nine", "ten"], correctAnswer:"seven"},
+  {question:"What broke the curse of sleeping beauty?", answers: ["sleep", "exercise", "makeup", "a kiss"], correctAnswer:"a kiss"},
+  {question:"Land before time was about?", answers: ["lions", "dinosaurs", "lobsters", "humans"], correctAnswer:"dinosaurs"},
+  {question:"Simba fell in love with...", answers: ["Amy", "Clara", "Nala", "Bella"], correctAnswer:"Nala"},
+  {question:"Finish the title: Beauty and the...", answers: ["Rabbit", "Ogre", "Prince", "Beast"], correctAnswer:"Beast"}];
 
-function endGame (){
+  function endGame (){
   clearInterval(timerId);
   quizDiv.style.display = "none";
+  endDiv.style.display = "block"
   scoreEl.textContent = "Your Score:" + userScore + "/10";
-}
+
+    quizDiv.addEventListener("click", function(event){
+      let element = event.target;
+      if (element.matches("button")){
+
+        localStorage.getItem("userScore", JSON.stringify(userScore));
+      }
+    })
+  }
   renderQuestion()
 
   function renderQuestion(){
@@ -81,6 +96,8 @@ function endGame (){
         userScore++;
         scoreEl.textContent = userScore + ("/10")
         console.log(userScore + "/10")
+
+        localStorage.setItem("userScore", JSON.stringify(userScore));
       }else {
         console.log("whoops")
         timer -=5
@@ -88,6 +105,7 @@ function endGame (){
 
       if(currentQuestion >= examQuestions.length){
         endGame()
+
     
       }else{
         renderQuestion();
